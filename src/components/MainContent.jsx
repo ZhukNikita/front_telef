8640193ea3aec51aafd4692623f5f647950afcd4
Tech5 <Tech5@office.local>
2023-08-15 15:42:34 +0300

    commit

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -103,19 +103,19 @@ export default function MainContent() {
         </div>
         <div className={styles.localNumbers}>
           <div className={styles.content}>
-            <motion.h3 initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration : 0.6 ,delay: 0.3}}>Local numbers in 67 countries</motion.h3>
-            <motion.p initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration : 0.6 ,delay: 0.3}}>We provision and port single numbers, ranges and 100- or 1,000- blocks worldwide.
+            <motion.h3 initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration : 0.9 ,delay: 0.3}}>Local numbers in 67 countries</motion.h3>
+            <motion.p initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration : 0.9 ,delay: 0.3}}>We provision and port single numbers, ranges and 100- or 1,000- blocks worldwide.
                 Trunks will support Emergency calling, short code dialing and other local services just like
                 a national telephone service provider.
             </motion.p >
             <div className={styles.features}>
-              <motion.div className={styles.feature} initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration : 0.6 ,delay: 0.3}}>
+              <motion.div className={styles.feature} initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration : 0.9 ,delay: 0.3}}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
                   <path d="M12 2C6.486 2 2 6.486 2 12v4.143C2 17.167 2.897 18 4 18h1a1 1 0 0 0 1-1v-5.143a1 1 0 0 0-1-1h-.908C4.648 6.987 7.978 4 12 4s7.352 2.987 7.908 6.857H19a1 1 0 0 0-1 1V18c0 1.103-.897 2-2 2h-2v-1h-4v3h6c2.206 0 4-1.794 4-4 1.103 0 2-.833 2-1.857V12c0-5.514-4.486-10-10-10z"></path>
                 </svg>
                   <p>Don't do it alone. We will navigate the local telecom environment for you.</p>
                 </motion.div>
-                <motion.div className={styles.feature} initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration : 0.6 ,delay: 0.3}}>
+                <motion.div className={styles.feature} initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration : 0.9 ,delay: 0.3}}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
                   <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zM4 12c0-.899.156-1.762.431-2.569L6 11l2 2v2l2 2 1 1v1.931C7.061 19.436 4 16.072 4 12zm14.33 4.873C17.677 16.347 16.687 16 16 16v-1a2 2 0 0 0-2-2h-4v-3a2 2 0 0 0 2-2V7h1a2 2 0 0 0 2-2v-.411C17.928 5.778 20 8.65 20 12a7.947 7.947 0 0 1-1.67 4.873z"></path>
                 </svg>
